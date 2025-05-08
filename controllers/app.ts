d8050b188b195045caecb63ef75f44946ee48f81
Awelle <awele.osuka@gmail.com>
2025-05-08 09:43:18 +0100

    added update user

--- controllers/app.ts
+++ controllers/app.ts
@@ -12,83 +12,91 @@ export class AppController {
  
 public async updateUser(req: Request & {user: any}, res: Response) {
 
-  const required = [
-    { name: 'name', type: 'string' },
-    { name: 'username', type: 'string' },
+  console.log(req.body, 'body')
 
-  ]
-  const { body } = req;
-  const hasRequired = utils.helpers.validParam(body, required)
-
-  if (hasRequired.success) {
-    console.log(req.user.email, 'rr')
-    let email: string = req.user.email.toLowerCase();
-
-    Jobseeker.findOne({email}, async (err:Error, user) => {
-        if (err){
-            console.log(err, 'user signup error');
-            return utils.helpers.sendErrorResponse(res, { }, 'Something went wrong, please try again')
-        }
-
-       if(user != null){ 
-
-               
-            Jobseeker.updateOne(
-              { email: email}, {
-              $set: {
-                  name: body.name,
-                  username: body.username
-              },
-          }, (err, updated) => {
-          
-              if (err) {
-                  console.log(err);
-                  return utils.helpers.errorResponse(
-                    res,
-                    [],
-                    'Something went wrong, please try again',
-                    )
-              }
-              
                   return utils.helpers.sendSuccessResponse(
                     res,
                     [],
                     'User has been updated',
                     )
 
+  // const required = [
+  //   { name: 'name', type: 'string' },
+  //   { name: 'username', type: 'string' },
+
+  // ]
+  // const { body } = req;
+  // const hasRequired = utils.helpers.validParam(body, required)
+
+  // if (hasRequired.success) {
+  //   console.log(req.user.email, 'rr')
+  //   let email: string = req.user.email.toLowerCase();
+
+  //   Jobseeker.findOne({email}, async (err:Error, user) => {
+  //       if (err){
+  //           console.log(err, 'user signup error');
+  //           return utils.helpers.sendErrorResponse(res, { }, 'Something went wrong, please try again')
+  //       }
+
+  //      if(user != null){ 
+
+               
+  //           Jobseeker.updateOne(
+  //             { email: email}, {
+  //             $set: {
+  //                 name: body.name,
+  //                 username: body.username
+  //             },
+  //         }, (err, updated) => {
+          
+  //             if (err) {
+  //                 console.log(err);
+  //                 return utils.helpers.errorResponse(
+  //                   res,
+  //                   [],
+  //                   'Something went wrong, please try again',
+  //                   )
+  //             }
+              
+  //                 return utils.helpers.sendSuccessResponse(
+  //                   res,
+  //                   [],
+  //                   'User has been updated',
+  //                   )
+
 
              
-            });
+  //           });
 
 
              
 
 
-        }else{
+  //       }else{
 
-            return utils.helpers.errorResponse(
-              res,
-              [],
-              'User does not exists',
-              )
+  //           return utils.helpers.errorResponse(
+  //             res,
+  //             [],
+  //             'User does not exists',
+  //             )
       
   
-        }
+  //       }
             
         
-    })
+  //   })
   
-  }else{
+  // }else{
 
-    console.log(hasRequired.message)
-    const message = hasRequired.message
-  return utils.helpers.sendErrorResponse(
-    res,
-    { message },
-    'Missing required fields',
-    )
+  //   console.log(hasRequired.message)
+  //   const message = hasRequired.message
+  // return utils.helpers.sendErrorResponse(
+  //   res,
+  //   { message },
+  //   'Missing required fields',
+  //   )
 
-  }
+  // }
 }
 
 
--- dist/controllers/app.js
+++ dist/controllers/app.js
@@ -11,7 +11,6 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppController = void 0;
 const utils_1 = require("../utils");
-const jobseeker_1 = require("../models/jobseeker");
 const stripe_1 = require("stripe");
 const stripe = new stripe_1.default('sk_test_26PHem9AhJZvU623DfE1x4sd', {
     apiVersion: '2023-10-16',
@@ -19,44 +18,61 @@ const stripe = new stripe_1.default('sk_test_26PHem9AhJZvU623DfE1x4sd', {
 class AppController {
     updateUser(req, res) {
         return __awaiter(this, void 0, void 0, function* () {
-            const required = [
-                { name: 'name', type: 'string' },
-                { name: 'username', type: 'string' },
-            ];
-            const { body } = req;
-            const hasRequired = utils_1.default.helpers.validParam(body, required);
-            if (hasRequired.success) {
-                console.log(req.user.email, 'rr');
-                let email = req.user.email.toLowerCase();
-                jobseeker_1.Jobseeker.findOne({ email }, (err, user) => __awaiter(this, void 0, void 0, function* () {
-                    if (err) {
-                        console.log(err, 'user signup error');
-                        return utils_1.default.helpers.sendErrorResponse(res, {}, 'Something went wrong, please try again');
-                    }
-                    if (user != null) {
-                        jobseeker_1.Jobseeker.updateOne({ email: email }, {
-                            $set: {
-                                name: body.name,
-                                username: body.username
-                            },
-                        }, (err, updated) => {
-                            if (err) {
-                                console.log(err);
-                                return utils_1.default.helpers.errorResponse(res, [], 'Something went wrong, please try again');
-                            }
-                            return utils_1.default.helpers.sendSuccessResponse(res, [], 'User has been updated');
-                        });
-                    }
-                    else {
-                        return utils_1.default.helpers.errorResponse(res, [], 'User does not exists');
-                    }
-                }));
-            }
-            else {
-                console.log(hasRequired.message);
-                const message = hasRequired.message;
-                return utils_1.default.helpers.sendErrorResponse(res, { message }, 'Missing required fields');
-            }
+            console.log(req.body, 'body');
+            return utils_1.default.helpers.sendSuccessResponse(res, [], 'User has been updated');
+            // const required = [
+            //   { name: 'name', type: 'string' },
+            //   { name: 'username', type: 'string' },
+            // ]
+            // const { body } = req;
+            // const hasRequired = utils.helpers.validParam(body, required)
+            // if (hasRequired.success) {
+            //   console.log(req.user.email, 'rr')
+            //   let email: string = req.user.email.toLowerCase();
+            //   Jobseeker.findOne({email}, async (err:Error, user) => {
+            //       if (err){
+            //           console.log(err, 'user signup error');
+            //           return utils.helpers.sendErrorResponse(res, { }, 'Something went wrong, please try again')
+            //       }
+            //      if(user != null){ 
+            //           Jobseeker.updateOne(
+            //             { email: email}, {
+            //             $set: {
+            //                 name: body.name,
+            //                 username: body.username
+            //             },
+            //         }, (err, updated) => {
+            //             if (err) {
+            //                 console.log(err);
+            //                 return utils.helpers.errorResponse(
+            //                   res,
+            //                   [],
+            //                   'Something went wrong, please try again',
+            //                   )
+            //             }
+            //                 return utils.helpers.sendSuccessResponse(
+            //                   res,
+            //                   [],
+            //                   'User has been updated',
+            //                   )
+            //           });
+            //       }else{
+            //           return utils.helpers.errorResponse(
+            //             res,
+            //             [],
+            //             'User does not exists',
+            //             )
+            //       }
+            //   })
+            // }else{
+            //   console.log(hasRequired.message)
+            //   const message = hasRequired.message
+            // return utils.helpers.sendErrorResponse(
+            //   res,
+            //   { message },
+            //   'Missing required fields',
+            //   )
+            // }
         });
     }
 }
